7f0715969b225428596fe08d574d9e47fd289ab3
nvpletnev <nvpletnev@gmail.com>
2020-03-12 13:51:42 +0300

    Create springboot app from template

---
+++ src/main/java/ru/altagroup/timecontrol/TimecontrolApplication.java
@@ -0,0 +1,13 @@
+package ru.altagroup.timecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimecontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimecontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/altagroup/timecontrol/TimecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.altagroup.timecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
